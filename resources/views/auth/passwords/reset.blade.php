67492aa0ab6bb5ea8e648e08beff222944342e8d
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2024-06-06 18:44:01 +0700

    display improvements change pass,confirm pass, email, and reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('auth.layouts.app')
 
 @section('content')
 <div class="container">
@@ -6,7 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
