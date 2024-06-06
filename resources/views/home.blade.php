70b61bcea492cff8d0df866da4b6b0cb7eebf124
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2024-06-06 15:17:36 +0700

    addnavbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Vertical Layout with Navbar</h3>
+                    <p class="text-subtitle text-muted">Navbar will appear on the top of the page.</p>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Layout Vertical Navbar
+                            </li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
         </div>
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">About Vertical Navbar</h4>
+                </div>
+                <div class="card-body">
+                    <p>Vertical Navbar is a layout option that you can use with Mazer. </p>
+
+                    <p>In case you want the navbar to be sticky on top while scrolling, add
+                        <code>.navbar-fixed</code> class alongside with <code>.layout-navbar</code> class.
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Dummy Text</h4>
+                </div>
+            </div>
+        </section>
     </div>
-</div>
 @endsection
